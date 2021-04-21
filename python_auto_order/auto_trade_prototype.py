93d376efa53d17912cf8491b4f36b463096483d7
Hoon Kim <innamgo@naver.com>
2021-04-21 16:16:40 +0900

    Update auto_trade_prototype.py

--- python_auto_order/auto_trade_prototype.py
+++ python_auto_order/auto_trade_prototype.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 import psycopg2
 import pyupbit
 import os
@@ -7,7 +6,6 @@ from time import sleep
 import logging.handlers
 import logging
 import traceback
-
 log_handlers = [logging.handlers.RotatingFileHandler(filename='/home/coinbot/log.txt', maxBytes=1024*1024*10), logging.StreamHandler()]
 logging.basicConfig(level = logging.INFO, format = '%(asctime)s [%(levelname)s] : %(message)s', handlers = log_handlers)
 logger = logging.getLogger('auto_trade_logger')
@@ -88,15 +86,18 @@ while True:
             non_trade_list = upbit.get_order(auto_trade[0])
             wait_buy_trade = get_element_include(non_trade_list, 'side', 'ask') #매수 미체결 개수
             wait_sell_trade = get_element_include(non_trade_list, 'side', 'bid') #매도 미체결 개수
+            current_market_balance = upbit.get_balance(ticker=auto_trade[0]) #현재 해당 코인 잔고 조회
             current_unit_price = pyupbit.get_current_price(auto_trade[0])
-            if wait_buy_trade > 0 and wait_sell_trade == 0: #미체결 매도가 없고 매수가 있다면 바로 매도
+            if wait_buy_trade > 0 and wait_sell_trade == 0 and current_market_balance == 0: #미체결 매도가 없고 미체결 매수 있고 잔고도 0이라면 있다면 아무것도 안함
+                pass
+            elif wait_buy_trade == 0 and wait_sell_trade == 0 and current_market_balance > 0: #미체결 매도/매수가 없고 잔고가 있다면 매도
                 sell_unit_price = get_tick_size(pyupbit.get_current_price(auto_trade[0]), max_sell_limit_rate)
-                sell_order_result = upbit.sell_limit_order(auto_trade[0], sell_unit_price, auto_trade['volume'])
+                sell_order_result = upbit.sell_limit_order(auto_trade[0], sell_unit_price, current_market_balance)
                 insert_trade_transaction_log('sell', auto_trade[0], sell_order_result)
-            elif wait_buy_trade == 0 and wait_sell_trade == 0: #미체결 매수/매도가 없다면 최대 정해진 금액 이하로 매수
+            elif wait_buy_trade == 0 and wait_sell_trade == 0 and current_market_balance == 0: #미체결 매수/매도가 없다면 최대 정해진 금액 이하로 매수
                 buy_order_result = upbit.buy_limit_order(auto_trade[0], current_unit_price, round(max_buy_limit / current_unit_price,6))
                 insert_trade_transaction_log('buy', auto_trade[0], buy_order_result)
-            elif wait_buy_trade == 0 and wait_sell_trade > 0: #미체결 매수가 없고 매도만 있다면 정해진 시간이 지난 후 자동매매 대상에서 삭제 미체결 주문도 취소
+            elif wait_buy_trade == 0 and wait_sell_trade > 0 and current_market_balance == 0: #미체결 매수가 없고 매도만 있다면 정해진 시간이 지난 후 자동매매 대상에서 삭제 미체결 주문도 취소
                 loop_auto_trade_second = loop_auto_trade_second + wait_second
                 if loop_auto_trade_second >= max_auto_trade_sceond:
                     cancel_uuid = get_element_value(non_trade_list, 'side', 'bid', 'uuid')
@@ -104,7 +105,12 @@ while True:
                     insert_trade_transaction_log('cancel', auto_trade[0], cancel_order_result)
                     delete_auto_trade_market(auto_trade[0])
                     loop_auto_trade_second = 0
-        logger.debug('waiting seconds : ' + str(wait_second))
+            else: # 이건 무슨 상황
+                logger.info('wait_buy_trade : ' + str(wait_buy_trade))
+                logger.info('wait_sell_trade : ' + str(wait_sell_trade))
+                logger.info('current_market_balance : ' + str(current_market_balance))
+
+        logger.info('waiting seconds : ' + str(wait_second))
         sleep(wait_second)
     except Exception as ex:
         logger.error(str(ex))
@@ -124,11 +130,4 @@ ret=upbit.get_order("KRW-XRP")
 print(ret)
 #print(upbit.cancel_order(ret[0]['uuid']))
 print(pyupbit.get_current_price("KRW-XRP"))
-
-CREATE TABLE public.trade_transaction_log (
-	transaction_type varchar(30) NULL,
-	market_coin varchar(30) NULL,
-	response_json json NULL,
-	create_date timestamp NULL DEFAULT CURRENT_TIMESTAMP
-);
 """
