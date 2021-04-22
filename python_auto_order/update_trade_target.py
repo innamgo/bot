9149e1c13b840cf357502c8b21c195ce7159353a
Hoon Kim <innamgo@naver.com>
2021-04-22 22:38:11 +0900

    Update update_trade_target.py

--- python_auto_order/update_trade_target.py
+++ python_auto_order/update_trade_target.py
@@ -1,13 +1,15 @@
 import os
 import time
 from time import sleep
+from slack import WebClient
+from slack.errors import SlackApiError
 import psycopg2
 import logging.handlers
 import logging
 import traceback
-log_handlers = [logging.handlers.RotatingFileHandler(filename='/home/coinbot/slack_log.txt', maxBytes=1024*1024*10), logging.StreamHandler()]
-logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s [%(levelname)s] : %(message)s', handlers = log_handlers)
-logger = logging.getLogger('slack_logger')
+log_handlers = [logging.handlers.RotatingFileHandler(filename='/home/coinbot/update_trade_target_log.txt', maxBytes=1024*10), logging.StreamHandler()]
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s [%(levelname)s] : %(message)s', handlers = log_handlers)
+logger = logging.getLogger('trade_logger')
 
 start = time.time()
 getting_interval = 15
@@ -16,7 +18,7 @@ max_reset_auto_trade_count = 30
 delete_auto_trade_count = 0
 loop_count = 0
 plus_price_delta_select_query = f"""
-select  delta_total.ticker_name||'('||get_code('market',delta_total.ticker_name)||')' ticker_name
+select  delta_total.ticker_name ticker_name
 	, coalesce(total_avg_delta,0) total_avg_delta, coalesce(total_cnt_delta,0) total_cnt_delta, coalesce(plus_avg_delta,0) plus_avg_delta
 	, coalesce(plus_cnt_delta,0) plus_cnt_delta, coalesce(minus_avg_delta,0) minus_avg_delta, coalesce(minus_cnt_delta,0) minus_cnt_delta
     ,get_code('market',delta_total.ticker_name) korean_name
@@ -46,7 +48,7 @@ on delta_total.ticker_name = delta_minus.ticker_name
 where total_avg_delta >= {alarm_threshold} and plus_cnt_delta >= 7
 """
 minus_price_delta_select_query = f"""
-select  delta_total.ticker_name||'('||get_code('market',delta_total.ticker_name)||')' ticker_name
+select  delta_total.ticker_name ticker_name
 	, coalesce(total_avg_delta,0) total_avg_delta, coalesce(total_cnt_delta,0) total_cnt_delta, coalesce(plus_avg_delta,0) plus_avg_delta
 	, coalesce(plus_cnt_delta,0) plus_cnt_delta, coalesce(minus_avg_delta,0) minus_avg_delta, coalesce(minus_cnt_delta,0) minus_cnt_delta
     ,get_code('market',delta_total.ticker_name) korean_name
@@ -79,8 +81,7 @@ conn = psycopg2.connect(host='localhost', dbname='botdb', user='coinbot', passwo
 conn.autocommit = True
 cur = conn.cursor()
 slack_token = os.environ['xoxb']
-if slack_token == '':
-    slack_token = ""
+client = WebClient(token=slack_token)
 
 def insert_code_group(group_name, code_key, code_value_char_1, code_value_int_1):
     insert_code_group_query = f"""
@@ -120,9 +121,17 @@ while True:
         alarm_list = cur.fetchall()
         logger.debug(alarm_list)
         for alarm in alarm_list:
-            coin_message = f"{alarm[0]} 코인이 최근 {getting_interval}분간  {alarm[3]} % 등락이 있었습니다. https://news.google.com/search?q={alarm[7]}&hl=ko&gl=KR&ceid=KR:ko"
-            logger.debug(coin_message)
-            insert_code_group('auto_order', alarm[0], 'on', 1)
+            try:
+                coin_message = f"{alarm[0]} 코인이 최근 {getting_interval}분간  {alarm[3]} % 등락이 있었습니다. https://news.google.com/search?q={alarm[7]}&hl=ko&gl=KR&ceid=KR:ko"
+                print(coin_message)
+                response = client.chat_postMessage(
+                    channel="showmethemoney",
+                    text="상승 중 : " + coin_message
+                )
+                insert_code_group('auto_order', alarm[0], 'on', 1)
+            except SlackApiError as e:
+                # You will get a SlackApiError if "ok" is False
+                assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
 
         cur.execute(minus_price_delta_select_query)
         logger.debug(minus_price_delta_select_query)
@@ -131,6 +140,11 @@ while True:
         for alarm in alarm_list:
             delete_auto_trade_count = delete_auto_trade_count + 1
             coin_message = f"{alarm[0]} 코인이 최근 {getting_interval}분간  {alarm[5]} % 등락이 있었습니다. https://news.google.com/search?q={alarm[7]}&hl=ko&gl=KR&ceid=KR:ko"
+            print(coin_message)
+            response = client.chat_postMessage(
+                channel="showmethemoney",
+                text="하락 중 : " + coin_message
+            )
             logger.debug(coin_message)
             if delete_auto_trade_count >= max_reset_auto_trade_count: #다수의 코인이 폭락하면 자동 매매를 종료 한다.
                 delete_auto_trade_market()
@@ -142,23 +156,3 @@ while True:
 cur.close()
 conn.close()
 
-"""
-
-
-insert into code_group (group_name , code_key , code_value_char_1, code_value_int_1)
-select 'auto_order_except', 'KRW-'||currency , 'on', 1
-from( 
-select replace(cast(aaa.ele->'currency' as varchar),'"','') as currency, replace(cast(aaa.ele->'balance' as varchar),'"','') as balance
-   , replace(cast(aaa.ele->'avg_buy_price' as varchar),'"','') as price
-from 
-(select JSON_ARRAY_ELEMENTS(response_json)  ele from crawling_upbit_fact where sequence_number=3) aaa
-) bbb;
-
-
-
-select sum(cast(replace(cast(response_json ->'price' as varchar),'"','') as float))
-from trade_transaction_log where transaction_type ='buy';
--
-select sum(cast(replace(cast(response_json ->'price' as varchar),'"','') as float))
-from trade_transaction_log where transaction_type ='sell';
-"""
